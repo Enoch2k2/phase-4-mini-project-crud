b3ebde41b83b97a14438b555331171d738f83835
Enoch2k2 <enoch2k2@hotmail.com>
2023-02-02 16:55:36 -0600

    demo done

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
+  resources :spices, except: [:show]
+  # get "/spices", to: "spices#index" #return an array of all spices
+  # post "/spices", to: "spices#create" #create a new spice
+  # patch "/spices/:id", to: "spices#update" #update an existing spice
+  # delete "/spices/:id", to: "spices#destroy" #delete an existing spice
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
